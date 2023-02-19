0e4c592b6bd5c52250418d08d9940ad35d518a07
gaberay2022 <gaberay2002@yahoo.com>
2023-02-19 16:26:45 -0600

    didn't have correct name to run app in Procfile

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+
 Lasertag = Flask(__name__)
 
 @Lasertag.route('/')
 def home():
     return render_template('PlayerEntryScreen.html')
+
 if __name__ == '__main__':
     Lasertag.run()
