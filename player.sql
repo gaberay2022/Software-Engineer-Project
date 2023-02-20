b2bb0bc49b0a80c11444543d7aad090e2d8ade9a
gaberay2022 <gaberay2002@yahoo.com>
2023-02-19 18:31:39 -0600

    testing database connection

--- player.sql
+++ player.sql
@@ -10,3 +10,5 @@ CREATE TABLE player (
 INSERT INTO player (id, first_name, last_name, codename)
 VALUES (1, 'Jim', 'Strother', 'Opus');
 
+INSERT INTO player (id, first_name, last_name, codename)
+VALUES (2, 'Gabe', 'Garcia', 'Void');
