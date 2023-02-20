9d0de85a5021756dd17f2d05fba02d96a6c302b9
gaberay2022 <gaberay2002@yahoo.com>
2023-02-19 18:50:42 -0600

    further testing of database to html

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ from flask import Flask, render_template
 import os
 import psycopg2
 
-conn = psycopg2.connect("postgres://ospqdsgchpdlxv:119a2e98fbf353a9cbc03a25dbe4ed8cc8ff4d22fbc377e200399084507f7506@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d4hsfg0607dsnf", sslmode = 'require') 
 
 Lasertag = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect("postgres://ospqdsgchpdlxv:119a2e98fbf353a9cbc03a25dbe4ed8cc8ff4d22fbc377e200399084507f7506@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d4hsfg0607dsnf", sslmode = 'require') 
+
+    return conn
+
 @Lasertag.route('/')
 def home():
-    return render_template('PlayerEntryScreen.html')
+    #return render_template('PlayerEntryScreen.html')
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM player;')
+    player = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', player=player)
 
 if __name__ == '__main__':
     Lasertag.run()
