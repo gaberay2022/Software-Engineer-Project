f3cd3f2139421503fd923222414c0b6bc97b5315
gaberay2022 <gaberay2002@yahoo.com>
2023-02-19 18:36:26 -0600

    continued test on database connection

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['postgres://ospqdsgchpdlxv:119a2e98fbf353a9cbc03a25dbe4ed8cc8ff4d22fbc377e200399084507f7506@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d4hsfg0607dsnf']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode = 'require') 
+conn = psycopg2.connect('postgres://ospqdsgchpdlxv:119a2e98fbf353a9cbc03a25dbe4ed8cc8ff4d22fbc377e200399084507f7506@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d4hsfg0607dsnf', sslmode = 'require') 
 
 Lasertag = Flask(__name__)
 
